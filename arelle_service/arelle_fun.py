2763ed88ba6e874bf5faa0fa675492de424d4cfd
Evan Prodromou <evan@openearth.org>
2025-02-11 11:50:33 -0500

    fix: debug output in arelle_fun

--- arelle_service/arelle_fun.py
+++ arelle_service/arelle_fun.py
@@ -1,5 +1,9 @@
 import os
 import sys
+import logging
+
+logger = logging.getLogger("uvicorn.error")
+logger.setLevel(logging.DEBUG)
 
 # Only imports for modules distributed as part of the standard Python library may go above this line.
 # All other imports will result in module not found exceptions on the frozen macOS build.
@@ -14,6 +18,7 @@ from arelle.BetaFeatures import BETA_OBJECT_MODEL_FEATURE, enableNewObjectModel
 from arelle import CntlrCmdLine, CntlrComServer
 
 def convert(args):
+    logger.debug("Convert called with args = " + args)
     internetConnectivityArgPattern = rf'--({INTERNET_CONNECTIVITY}|{INTERNET_CONNECTIVITY.lower()})'
     internetConnectivityArgRegex = re.compile(internetConnectivityArgPattern)
     internetConnectivityOfflineEqualsRegex = re.compile(f"{internetConnectivityArgPattern}={OFFLINE}")
@@ -27,16 +32,25 @@ def convert(args):
             break
         prevArg = arg
 
+    logger.debug("prevArg = " + prevArg)
     # Model transition must be enabled before any other imports to avoid mixing base classes.
     if f"--{BETA_OBJECT_MODEL_FEATURE}" in args or f"--{BETA_OBJECT_MODEL_FEATURE.lower()}" in args:
+        logger.debug("Enabling new object model")
         enableNewObjectModel()
 
     if '--COMserver' in args:
+        logger.debug("Turning on com server")
         CntlrComServer.main()
     elif __name__.startswith('_mod_wsgi_') or os.getenv('wsgi.version'):
+        logger.debug("Turning on wsgi application")
         application = CntlrCmdLine.wsgiApplication()
     elif __name__ in ('__main__', 'arelleCmdLine__main__', 'arellecmdline__main__','arelle_fun'): #cx_Freeze 5 prepends module name to __main__
+        logger.debug("running command line")
         os.environ["ARELLE_ARGS"] = ' '.join(args)
-        print(os.getenv("ARELLE_ARGS"))
+        logger.debug(os.getenv("ARELLE_ARGS"))
+
+        logger.debug("Calling main")
 
         CntlrCmdLine.main()
+
+        logger.debug("main() finished")
